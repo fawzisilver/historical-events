2f3e2822cf07c9b010e2ac0b2ab8abb9b7156252
fawzisilver <90504301+fawzisilver@users.noreply.github.com>
2024-10-23 10:59:42 -0600

    adding functionality and features to the map with additional css design

--- app/components/MapApplication.tsx
+++ app/components/MapApplication.tsx
@@ -3,12 +3,13 @@ import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet"
 import 'leaflet/dist/leaflet.css'
 import { Icon } from 'leaflet'
 import { historyEvents, HistoricalEvents } from "../historyEvents"
-import { useState,} from "react"
+import { useState, useRef } from "react"
+import { Map as Leaflet } from "leaflet"
 
 const defaultPosition : [number, number] = [51.505, -0.09];
 
-const emptyStar = <i className="fa-regular fa-star"></i>;  // icon tag from html
-const fullStar = <i className="fa-solid fa-star" style={{ color: "#fdc401"}}></i>
+const emptyHeart = <i className="fa-regular fa-heart"></i>;  // icon tag from html
+const fullHeart = <i className="fa-solid fa-heart" style={{ color: "rgb(156, 54, 54)"}}></i>
 // mapsapp
 const MapApplication = () => {
 
@@ -20,6 +21,8 @@ const MapApplication = () => {
   });
 
   console.log(favorites)
+
+  const mapRef = useRef<Leaflet | null>(null);
 
   const icon: Icon = new Icon({
     iconUrl: "marker.svg",
@@ -41,39 +44,52 @@ const MapApplication = () => {
 
   }
 
+  const handleEventHover = (eventPosition : [number, number]) => {
+    const map = mapRef.current;
+
+    if(map && eventPosition) {
+      map.flyTo(eventPosition, 15, { duration: 2.5 });
+    }
+  }
+
   return (
     <div className="flex flex-col gap-5 w-full h-full">
   {/* Navigation Bar */}
-  <div className="nav h-[8%] flex items-center justify-center ">
-    <h1>Navigation Bar</h1>
-  </div>
+    <div className="nav h-[8%] flex items-center justify-center ">
+      <h1>Navigation Bar</h1>
+    </div>
 
   {/* Main Content with Sidebar on Left and Map on Right */}
   <div className="flex w-full h-[90%]">
     
     {/* Sidebar for Favorite Events on the Left */}
-    <div className="w-1/5 h-full liked-events">
-      <h2 className="liked_events__title text-white text-center">
-        <i className="fa-solid fa-star justify-center"></i> Favorite Events
+    {favorites.length > 0 ? (
+      <div className="w-1/5 h-full liked-events">
+      <h2 className="liked_events__title text-lg text-[var(--color-beige)] mb-2 px-2 rounded text-center font-bold ">
+        <i className="fa-solid fa-heart justify-center"></i> Favorite Events
       </h2>
-      <ul className="text-white">
-        {favorites.map((id) => {
-          const event = historyEvents.find((event) => event.id === id);
-          if (event) {
-            return (
-              <li key={event.id}>
-                <h3>{event.title}</h3>
-              </li>
-            );
+      <ul>
+          {favorites
+          .map(id => {
+            return historyEvents.find(event => event.id === id) //array of events that matched
+          })
+
+          .filter(event => event !== undefined) // event with values (as array)
+          .map(event => (
+            <li className="liked-events__event" key={event?.id} onMouseEnter={() => handleEventHover(event!.position)}>
+              <h3 className="liked-events__eventTitle">{event?.title}</h3>
+            </li>
+          ))
+          
           }
-          return null;
-        })}
-      </ul>
+        </ul>
     </div>
+    ) : null }
+    
 
     {/* Main Content - Map on the Right */}
-    <div className="w-4/5 h-full pl-4">
-      <MapContainer center={defaultPosition} zoom={13} className="map-container">
+    <div className={`${favorites.length > 0 ? 'w-4/5' : 'w-full'} h-full pl-4`}>
+      <MapContainer center={defaultPosition} zoom={13} className="map-container" ref={mapRef}>
         <TileLayer url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png" />
         {historyEvents.map((event) => (
           <Marker
@@ -97,7 +113,7 @@ const MapApplication = () => {
                 <p>{activeEvent.description}</p>
                 <button onClick={() => handleFavorite(activeEvent.id)} className="popup-inner__button">
                   <span>
-                    {favorites.includes(activeEvent.id) ? fullStar : emptyStar}
+                    {favorites.includes(activeEvent.id) ? fullHeart : emptyHeart}
                     {favorites.includes(activeEvent.id) ? ' Unfavorite' : ' Favorite'}
                   </span>
                 </button>
